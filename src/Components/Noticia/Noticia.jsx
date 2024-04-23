66e70b7d06b596ecbfe6858438a0e296bf2b9fe4
jorge <andreselpinta2006@hotmail.com>
2024-04-23 17:44:59 -0500

    tercer commit

--- src/Components/Noticia/Noticia.jsx
+++ src/Components/Noticia/Noticia.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-export const Noticia = ({idNews,img,date,tittle,Description}) => {
+export const Noticia = ({ idNews, img, date, Description }) => {
   return (
-    <div id={idNews}>
-    <div>{img}</div>
-    <div className='w-full h-12'>
-   <p>{date}</p>
-   <h1>{tittle}</h1>
-   <h1>{Description}</h1>
-    
-   </div>
+    <div className="">
+      <div className='flex flex items-center'  id={idNews}>
+        <div><img className='p-10' src={img} alt="Imagen de la noticia" /></div>
+        <hr className='color-black' />
+        <div className='flex flex-col '>
+          <p className='text-black'>{date}</p>
+          <p className='black '>{Description}</p>
+          
+          <hr className=''/>
+          
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import { Noticia } from '../Noticia/Noticia'
 import { extras } from '../Noticias/Noticias'
-
+import new2 from '../../assets/Images/Dos.png'
 export const Section = () => {
   return (
  <>
- <section className='bg-black h-80 w-2/5'>
-{extras.map(noticia => (
+ <section className='bg-black h-auto w-2/5'>
+
+<img src={new2} alt="" className='p-10 pl-28  ' />
+<h1></h1>
+<p className='text-white'>16 abril 2024</p>
+<h2 className='text-white'>El Barça se despide de la Champions tras caer frente al PSG (1-4) en un partido marcado por la expulsión de Ronald Araújo La expulsión de Araujo y el penalti de Cancelo marcaron un partido en el que los culés empezaron con ventaja (1-4)</h2>
+</section>
+ <section className='bg-blue-700 h-3/6 w-2/5 border-white'>
+ {extras.map(noticia => (
   <Noticia 
   idNews={noticia.idNews}
   img={noticia.img}
   date={noticia.date}
   tittle={noticia.tittle}
-  Description={noticia.Description}/>
+  Description={noticia.descripcion}/>
 ))
 
 }
- </section>
- <section className='bg-blue-700 h-80 w-2/5 border-white'>
-
  </section>
  </>
   )
